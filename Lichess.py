9746d8d3715fb48875c26ad0b48514471f3cddf3
lakvinu <sandeepalhah@gmail.com>
2021-08-15 13:54:37 +1000

    added reconnect feature

--- Lichess.py
+++ Lichess.py
@@ -1,82 +1,114 @@
-import berserk
+import requests
+import json
 from Game import Game
 import chess
 
-
 token = "wh5gZCgBsplPIIVN"
 
-session = berserk.TokenSession(token)
+headers = {'Authorization': f'Bearer {token}'}
 
-client = berserk.Client(session)
+game_state_url = 'https://lichess.org/api/stream/event'
+
+game_id = 'placeholder'
+
+is_checkmate = False
+bot_challenges = False
+
+bot = Game()
+
+while True:
+
+    state_session = requests.Session()
+    request = state_session.get(game_state_url, headers=headers, stream=True)
+
+    for line in request.iter_lines():
+        if line:
+            #{'type': 'challenge', 'challenge': {'id': 'M8gMUPgc', 'url': 'https://lichess.org/M8gMUPgc', 'status': 'created', 'challenger': {'id': 'lakvinu', 'name': 'lakvinu', 'title': None, 'rating': 1681, 'provisional': True, 'online': True}, 'destUser': {'id': 'lakvinbot', 'name': 'LakvinBot', 'title': 'BOT', 'rating': 1500, 'provisional': True, 'online': True}, 'variant': {'key': 'standard', 'name': 'Standard', 'short': 'Std'}, 'rated': False, 'speed': 'rapid', 'timeControl': {'type': 'clock', 'limit': 1200, 'increment': 0, 'show': '20+0'}, 'color': 'white', 'perf': {'icon': '\ue017', 'name': 'Rapid'}}}
+            bot = Game()
+            bot.board = chess.Board()
+            challenge_json = json.loads(line)
+
+            if challenge_json['type'] == 'challenge':
+
+                print('You have received challenge!')
+                challenge_id = challenge_json['challenge']['id']
+                challenger = challenge_json['challenge']['challenger']['id']
+
+                if challenge_json['challenge']['variant']['key'] != 'standard':
+                    requests.post('https://lichess.org/api/challenge/' + challenge_id + '/decline', headers={'Authorization': f'Bearer {token}'})
+                    print("Challenge Declined")
+                    continue
+
+                else:
+                    requests.post('https://lichess.org/api/challenge/' + challenge_id + '/accept', headers={'Authorization': f'Bearer {token}'})
+                    print("Challenge Accepted")
+
+            second_session = requests.Session()
+            request = second_session.get(game_state_url, headers=headers, stream=True)
+
+            for line in request.iter_lines():
+                if line:
+                    game_start_json = json.loads(line)
+                    if 'game' not in game_start_json:
+                        continue
+
+                    game_id = game_start_json['game']['id']
+                    break
+
+            game_stream_url = 'https://lichess.org/api/bot/game/stream/' + game_id
+
+            bot_move_url = 'https://lichess.org/api/bot/game/' + game_id + '/move/'
+
+            s = requests.Session()
+            r = s.get(game_stream_url, headers=headers, stream=True)
+
+            my_turn = chess.WHITE
+            opp_turn = False
+            is_ignore = False
+            game_end = False
+            for line in r.iter_lines():
+                if line:
+                    state = json.loads(line)
+
+                    if "id" in state:
+                        if state['black']['id'] == 'lakvinbot':
+                            my_turn = chess.BLACK
+
+                        moves = state['state']['moves'].split()
+
+                        for move in moves:
+                            bot.move(move)
+
+                        if bot.board.turn == my_turn:
+                            best_move = bot.find_best_move()
+                            requests.post(bot_move_url + best_move, headers={'Authorization': f'Bearer {token}'})
+                            is_ignore = True
+
+
+                    else:
+                        if state['status'] != "started":
+                            game_end = True
+                            break
+
+                        if not is_ignore:
+                            bot.move(state['moves'].split()[-1])
+                            best_move = bot.find_best_move()
+                            requests.post(bot_move_url + best_move, headers={'Authorization': f'Bearer {token}'})
+                            is_ignore = True
+
+                        else:
+                            is_ignore = False
+
+            if game_end:
+                print("Game Finish")
+                break
 
 
 
-class Manage:
-
-
-    def __init__(self, game_id):
-        self.game_id = game_id
-        self.stream = client.bots.stream_game_state(game_id=game_id)
-        self.current_state = next(self.stream)
-        self.colour = chess.WHITE if self.current_state['white']['id'] == 'lakvinbot' else chess.BLACK
-        self.bot = Game()
-
-
-    def run(self):
-        if self.whose_turn() == self.colour:
-            self.my_move()
-
-        for event in self.stream:
-            if event['type'] == 'gameState':
-                self.handle_state_change(event)
-
-
-    def handle_state_change(self, event):
-        is_turn = self.decide_turn(event) == self.colour
-
-        if is_turn:
-            self.do_opp_move(self.last_move(event))
-            self.my_move()
-
-
-    def whose_turn(self):
-        move_list = client.games.export(self.game_id)['moves'].split()
-
-        if len(move_list) % 2 == 0:
-            return chess.WHITE
-
-        return chess.BLACK
-
-    def decide_turn(self, event):
-        move_list = event['moves'].split()
-
-        if len(move_list) % 2 == 0:
-            return chess.WHITE
-
-        return chess.BLACK
-
-    def my_move(self):
-        move = self.bot.find_best_move()
-        client.bots.make_move(self.game_id, move)
-
-    def last_move(self, event):
-        return event['moves'].split()[-1]
-
-    def do_opp_move(self, move):
-        self.bot.move(move)
 
 
 
-in_game = False
-for event in client.bots.stream_incoming_events():
 
-    if event['type'] == 'challenge':
-        if not in_game:
-            print("Challenge Accepted!")
-            in_game = True
-            client.bots.accept_challenge(event['challenge']['id'])
 
-    elif event['type'] == 'gameStart':
-        manage = Manage(event['game']['id'])
-        manage.run()
+
 
